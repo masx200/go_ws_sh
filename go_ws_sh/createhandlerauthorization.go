69b5b91e124732df5eb4ae55e9148976295ad8cf
masx200 <masx200@qq.com>
2025-01-02 17:28:08 +0800

    Update createhandlerauthorization.go

--- go_ws_sh/createhandlerauthorization.go
+++ go_ws_sh/createhandlerauthorization.go
@@ -50,7 +50,7 @@ func createhandlerauthorization(TokenFolder string, credentials []Credentials /*
 		proto := r.Request.Header.Get("Sec-Websocket-Protocol")
 		if proto != "" {
 			for _, str := range strings.Split(proto, ",") {
-
+				log.Println("proto", str)
 				decoded, err := url.QueryUnescape(str)
 				if err != nil {
 					fmt.Printf("Error parsing input: %v\n", err)
@@ -72,75 +72,76 @@ func createhandlerauthorization(TokenFolder string, credentials []Credentials /*
 					return
 				} else if slices.Contains(slice.Map(credentials, func(credential Credentials) string { return credential.Username }), result["username"]) {
 					next(w, r)
-					return
+					// return
 				} else {
 					r.AbortWithMsg("Error: Unauthorized token is invalid", consts.StatusUnauthorized)
 					return
 				}
 			}
-		}
-		auth := r.Request.Header.Get("Authorization")
-		if auth == "" {
-			log.Println("No Authorization header")
-			r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
-			r.SetStatusCode(consts.StatusUnauthorized)
-			// r.AbortWithMsg("No Authorization header", consts.StatusUnauthorized)
-			r.WriteString("No Authorization header")
-			return
-		}
-		if !strings.HasPrefix(auth, "Basic ") {
-			log.Println("No Basic auth")
-			r.SetStatusCode(consts.StatusUnauthorized)
-			r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
-			r.WriteString("No Basic auth Unauthorized")
-			return
-		}
-
-		credential := strings.TrimPrefix(auth, "Basic ")
-		var rawcredential []byte
-		if rawcredential2, err := base64.StdEncoding.DecodeString(credential); err != nil {
-			r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
-			r.SetStatusCode(consts.StatusUnauthorized)
-			r.WriteString(err.Error())
-			return
 		} else {
-			rawcredential = rawcredential2
-		}
-		// fmt.Printf("credential: %v\n", string(rawcredential))
-		if _, ok := credentialsmap[string(rawcredential)]; !ok {
-			log.Println("Invalid credential", credential)
-			r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
-			r.SetStatusCode(consts.StatusUnauthorized)
-			r.WriteString("Invalid credential Unauthorized")
-			// r.AbortWithMsg("Invalid credential", consts.StatusUnauthorized)
-			return
-		}
 
-		Upgrade := strings.ToLower(r.Request.Header.Get("Upgrade"))
-		Connection := strings.ToLower(r.Request.Header.Get("Connection"))
-		//if !tokenListContainsValue(r.Request.Header, "Connection", "upgrade") {
-		if !strings.Contains(Connection, "upgrade") {
-			log.Println("Not a upgrade request")
-			r.NotFound() //http.NotFound(w, r)
-			return
-		}
-		if !strings.Contains(Upgrade, "websocket") {
-			log.Println("Not a websocket request")
-			// if !tokenListContainsValue(r.Header, "Upgrade", "websocket") {
-			r.NotFound() //http.NotFound(w, r)
-			return
-		}
+			auth := r.Request.Header.Get("Authorization")
+			if auth == "" {
+				log.Println("No Authorization header")
+				r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
+				r.SetStatusCode(consts.StatusUnauthorized)
+				// r.AbortWithMsg("No Authorization header", consts.StatusUnauthorized)
+				r.WriteString("No Authorization header")
+				return
+			}
+			if !strings.HasPrefix(auth, "Basic ") {
+				log.Println("No Basic auth")
+				r.SetStatusCode(consts.StatusUnauthorized)
+				r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
+				r.WriteString("No Basic auth Unauthorized")
+				return
+			}
 
-		if !r.IsGet() /* != http.MethodGet */ {
-			log.Println("Not a get request")
-			r.NotFound()
-			//http.NotFound(w, r)
-			return
+			credential := strings.TrimPrefix(auth, "Basic ")
+			var rawcredential []byte
+			if rawcredential2, err := base64.StdEncoding.DecodeString(credential); err != nil {
+				r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
+				r.SetStatusCode(consts.StatusUnauthorized)
+				r.WriteString(err.Error())
+				return
+			} else {
+				rawcredential = rawcredential2
+			}
+			// fmt.Printf("credential: %v\n", string(rawcredential))
+			if _, ok := credentialsmap[string(rawcredential)]; !ok {
+				log.Println("Invalid credential", credential)
+				r.Response.Header.Set("WWW-Authenticate", "Basic realm=\"go_ws_sh\"")
+				r.SetStatusCode(consts.StatusUnauthorized)
+				r.WriteString("Invalid credential Unauthorized")
+				// r.AbortWithMsg("Invalid credential", consts.StatusUnauthorized)
+				return
+			}
+
+			Upgrade := strings.ToLower(r.Request.Header.Get("Upgrade"))
+			Connection := strings.ToLower(r.Request.Header.Get("Connection"))
+			//if !tokenListContainsValue(r.Request.Header, "Connection", "upgrade") {
+			if !strings.Contains(Connection, "upgrade") {
+				log.Println("Not a upgrade request")
+				r.NotFound() //http.NotFound(w, r)
+				return
+			}
+			if !strings.Contains(Upgrade, "websocket") {
+				log.Println("Not a websocket request")
+				// if !tokenListContainsValue(r.Header, "Upgrade", "websocket") {
+				r.NotFound() //http.NotFound(w, r)
+				return
+			}
+
+			if !r.IsGet() /* != http.MethodGet */ {
+				log.Println("Not a get request")
+				r.NotFound()
+				//http.NotFound(w, r)
+				return
+			}
+			//httpServeMux.ServeHTTP(w, r)
+			next(w, r)
 		}
-		//httpServeMux.ServeHTTP(w, r)
-		next(w, r)
 	}
-
 }
 
 // parseKeyValuePairs 解析形如 "key1=value1; key2=value2; key3=value3" 的字符串
