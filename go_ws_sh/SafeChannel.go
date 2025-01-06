ec74aff93476ef091a46e296118b35545acd911f
masx200 <masx200@qq.com>
2025-01-06 21:27:06 +0800

    Update SafeChannel.go

--- go_ws_sh/SafeChannel.go
+++ go_ws_sh/SafeChannel.go
@@ -32,10 +32,12 @@ func (sc *SafeChannel[T]) IsClosed() bool {
 // Send 尝试向 channel 发送数据，如果 channel 已经关闭则返回 false。
 func (sc *SafeChannel[T]) Send(v T) bool {
 	sc.mu.Lock()
-	defer sc.mu.Unlock()
+
 	if sc.closed {
+		sc.mu.Unlock()
 		return false
 	}
+	sc.mu.Unlock()
 	// select {
 	//case
 	sc.ch <- v
@@ -51,11 +53,13 @@ func (sc *SafeChannel[T]) Send(v T) bool {
 // Receive 尝试从 channel 接收数据，如果 channel 已关闭并且没有更多数据，则返回零值和 false。
 func (sc *SafeChannel[T]) Receive() (T, bool) {
 	sc.mu.Lock()
-	defer sc.mu.Unlock()
+
 	if sc.closed && len(sc.ch) == 0 {
 		var zero T // 零值
+		sc.mu.Unlock()
 		return zero, false
 	}
+	sc.mu.Unlock()
 	v, ok := <-sc.ch
 	return v, ok
 }
