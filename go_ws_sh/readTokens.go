52cdb470e9e78d1ffb7d55141744de9395b1030d
masx200 <masx200@qq.com>
2025-03-22 14:10:29 +0800

    删除readTokens

--- go_ws_sh/readTokens.go
+++ go_ws_sh/readTokens.go
@@ -1,47 +1,124 @@
 package go_ws_sh
 
-import (
-	"encoding/json"
-	"os"
-)
+// import (
+// 	"encoding/json"
+// 	"os"
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// )
 
-// TokenStore 定义 Token 存储的结构体
-type TokenStoreList []TokenStore
+// // TokenStore 定义 Token 存储的结构体
+// type TokenStoreList []TokenStore
 
-// readTokens 函数读取指定路径的 JSON 文件，并将其解析为 TokenStore 类型的数组，同时返回可能出现的错误
-func readTokens(getfilepath func() (string, error)) (TokenStoreList, error) {
-	// 获取文件路径
-	filePath, err := getfilepath()
-	if err != nil {
-		// 返回错误，而不是使用 panic
-		return nil, err
-	}
-	if !FileExists(filePath) {
-		return nil, nil
-	}
-	// 打开文件
-	file, err := os.Open(filePath)
-	if err != nil {
-		// 返回错误，而不是使用 panic
-		return nil, err
-	}
-	defer file.Close()
+// // readTokens 函数读取指定路径的 JSON 文件，并将其解析为 TokenStore 类型的数组，同时返回可能出现的错误
+// func readTokens(getfilepath func() (string, error)) (TokenStoreList, error) {
+// 	// 获取文件路径
+// 	filePath, err := getfilepath()
+// 	if err != nil {
+// 		// 返回错误，而不是使用 panic
+// 		return nil, err
+// 	}
+// 	if !FileExists(filePath) {
+// 		return nil, nil
+// 	}
+// 	// 打开文件
+// 	file, err := os.Open(filePath)
+// 	if err != nil {
+// 		// 返回错误，而不是使用 panic
+// 		return nil, err
+// 	}
+// 	defer file.Close()
 
-	// 创建 JSON 解码器
-	var tokenStore TokenStoreList
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&tokenStore)
-	if err != nil {
-		// 返回错误，而不是使用 panic
-		return nil, err
-	}
+// 	// 创建 JSON 解码器
+// 	var tokenStore TokenStoreList
+// 	decoder := json.NewDecoder(file)
+// 	err = decoder.Decode(&tokenStore)
+// 	if err != nil {
+// 		// 返回错误，而不是使用 panic
+// 		return nil, err
+// 	}
 
-	return tokenStore, nil
-}
-func FileExists(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	return os.IsNotExist(err)
-}
+// 	return tokenStore, nil
+// }
+// func FileExists(path string) bool {
+// 	_, err := os.Stat(path)
+// 	if err == nil {
+// 		return true
+// 	}
+// 	return os.IsNotExist(err)
+// }
--- go_ws_sh/writeTokens.go
+++ go_ws_sh/writeTokens.go
@@ -1,40 +1,128 @@
 package go_ws_sh
 
-import (
-	"encoding/json"
-	"os"
-	"path/filepath"
-)
+// import (
+// 	"encoding/json"
+// 	"os"
+// 	"path/filepath"
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// )
 
-// WriteTokens 函数将 TokenStore 类型的数据写入到指定的文件中
-func WriteTokens(getfilepath func() (string, error), TokenStore TokenStore) error {
-	// 获取文件路径
-	filePath, err := getfilepath()
-	if err != nil {
-		return err
-	}
-	//创建出这个文件所在的文件夹
-	dir := filepath.Dir(filePath)
-	err = os.MkdirAll(dir, os.ModePerm)
-	if err != nil {
-		return err
-	}
-	// 创建或打开文件
-	file, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+// // WriteTokens 函数将 TokenStore 类型的数据写入到指定的文件中
+// func WriteTokens(getfilepath func() (string, error), TokenStore TokenStore) error {
+// 	// 获取文件路径
+// 	filePath, err := getfilepath()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	//创建出这个文件所在的文件夹
+// 	dir := filepath.Dir(filePath)
+// 	err = os.MkdirAll(dir, os.ModePerm)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	// 创建或打开文件
+// 	file, err := os.Create(filePath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer file.Close()
 
-	// 创建 JSON 编码器
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
+// 	// 创建 JSON 编码器
+// 	encoder := json.NewEncoder(file)
+// 	encoder.SetIndent("", "  ")
 
-	// 将 TokenStore 编码为 JSON 并写入文件
-	err = encoder.Encode(TokenStore)
-	if err != nil {
-		return err
-	}
+// 	// 将 TokenStore 编码为 JSON 并写入文件
+// 	err = encoder.Encode(TokenStore)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
