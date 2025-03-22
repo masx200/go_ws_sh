30e15b5263a28c04599ff9b1f9e1d5366bf4a00b
masx200 <masx200@qq.com>
2025-03-22 14:30:04 +0800

    refactor(config): 重构客户端配置并更新服务器配置
    - 更新客户端配置文件格式，移除不必要的用户名字段
    - 重新定义 ClientSession 和 CredentialsClient 结构体
    - 更新管道客户端代码以适应新的配置格式
    - 在服务器配置文件中添加 session_file 字段

--- go_ws_sh/ClientConfig.go
+++ go_ws_sh/ClientConfig.go
@@ -1,1 +1,34 @@
 package go_ws_sh
+
+import "fmt"
+
+type ClientSession struct {
+	Name string `json:"name"`
+}
+type CredentialsClient struct {
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Token      string `json:"token"`
+	Type       string `json:"type"`
+	Identifier string `json:"identifier"`
+}
+
+func (c CredentialsClient) String() string {
+	return fmt.Sprintf("CredentialsClient{Username: %s, Password: %s, Token: %s, Type: %s, Identifier: %s}",
+		c.Username, c.Password, c.Token, c.Type, c.Identifier)
+}
+
+type ConfigClient struct {
+	Credentials CredentialsClient      `json:"credentials"`
+	Sessions    ClientSession          `json:"sessions"`
+	Servers     ClientConfigConnection `json:"servers"`
+}
+
+type ClientConfigConnection struct {
+	Port     string `json:"port"`
+	Protocol string `json:"protocol"`
+	Ca       string `json:"ca"`
+	Insecure bool   `json:"insecure"`
+	Host     string `json:"host"`
+	IP       string `json:"ip"`
+}
