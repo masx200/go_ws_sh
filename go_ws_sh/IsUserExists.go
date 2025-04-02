feb14851b81bf58edbe56f93e39a03771722d111
masx200 <masx200@qq.com>
2025-04-02 23:09:20 +0800

    Update IsUserExists.go

--- go_ws_sh/IsUserExists.go
+++ go_ws_sh/IsUserExists.go
@@ -25,7 +25,7 @@ func IsUserExists(credentialdb *gorm.DB, username string) bool {
 // IsUserExists 判断指定 username 的用户是否存在于 credentialdb 中
 func IsSessionExists(sessiondb *gorm.DB, sessionname string) bool {
 	var count int64
-	err := sessiondb.Model(&CredentialStore{}).Where("name = ?", sessionname).Count(&count).Error
+	err := sessiondb.Model(&SessionStore{}).Where("name = ?", sessionname).Count(&count).Error
 	if err != nil {
 		log.Printf("Error checking if user exists: %v", err)
 		return false
