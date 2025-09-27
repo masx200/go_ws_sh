b09574b978b5b057f66e855eaf3825447fa31e56
masx200 <masx200@qq.com>
2025-09-27 18:36:54 +0100

    Update authorizationmiddleware.go

--- go_ws_sh/authorizationmiddleware.go
+++ go_ws_sh/authorizationmiddleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"strings"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
@@ -12,7 +13,6 @@ import (
 	"github.com/masx200/go_ws_sh/types"
 )
 
-
 // AuthorizationMiddleware 定义身份验证中间件
 func AuthorizationMiddleware(credentialdb *gorm.DB, tokendb *gorm.DB, sessiondb *gorm.DB) types.HertzMiddleWare {
 	return func(c context.Context, r *app.RequestContext, next types.HertzNext) {
@@ -20,6 +20,7 @@ func AuthorizationMiddleware(credentialdb *gorm.DB, tokendb *gorm.DB, sessiondb 
 		bearertoken := r.Request.Header.Get("authorization")
 
 		if bearertoken != "" {
+			bearertoken=strings.TrimPrefix(bearertoken, "Bearer ")
 			decoded, err := base64.StdEncoding.DecodeString(bearertoken)
 			if err != nil {
 				r.AbortWithStatusJSON(consts.StatusUnauthorized, map[string]string{
